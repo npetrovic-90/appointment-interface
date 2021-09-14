53d89c91c47ceaef0dd4f40273ea00db589af767
Nemanja Petrovic <nemanja90petrovic@hotmail.com>
2021-09-14 12:30:41 +0200

    Using axios lib to import data from json file

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,32 @@
 <template>
-  <div id='main-app' class='container'>
-    <h4>{{title}}</h4>
-    <font-awesome-icon icon='plus' class='me-2'></font-awesome-icon>Add appointment
+  <div id="main-app" class="container">
+    <h4>{{ title }}</h4>
+    <font-awesome-icon icon="plus" class="me-2"></font-awesome-icon>Add
+    appointment
+    <div v-for='(item,index) in appointments' v-bind:key='index'>
+      <h4>{{item.petName}}</h4>
+      <p>{{item.aptNotes}}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import axios from 'axios'
 export default {
-  name: 'MainApp',
+  name: "MainApp",
   data: function() {
     return {
-      title: "Appointment list"
-    }
+      title: "Appointment list",
+      appointments: []
+    };
   },
   components: {
-    FontAwesomeIcon
+    FontAwesomeIcon,
+  },
+  mounted() {
+    axios.get('./data/appointments.json')
+          .then(response=>(this.appointments = response.data))
   }
-
-}
+};
 </script>
-

11b3223948bfd19213b52f6e164d1cc081d814a0
Nemanja Petrovic <nemanja90petrovic@hotmail.com>
2021-09-14 12:13:00 +0200

    Cleaning up and testing/adding additional modules

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id='main-app' class='container'>
+    <h4>{{title}}</h4>
+    <font-awesome-icon icon='plus' class='me-2'></font-awesome-icon>Add appointment
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 export default {
-  name: 'App',
+  name: 'MainApp',
+  data: function() {
+    return {
+      title: "Appointment list"
+    }
+  },
   components: {
-    HelloWorld
+    FontAwesomeIcon
   }
+
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
